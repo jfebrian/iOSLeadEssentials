0b7893e56877ed9eb2240a2dbf9653c8aab6c585
Joanda Febrian <mail@joanda.dev>
2022-11-29 20:16:12 +0700

    Guarantee to only receive one error

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -5,8 +5,8 @@
 //  Created by Joanda Febrian on 29/11/22.
 //
 
-import XCTest
 import EssentialFeed
+import XCTest
 
 final class RemoteFeedLoaderTest: XCTestCase {
 
@@ -24,7 +24,7 @@ final class RemoteFeedLoaderTest: XCTestCase {
 
         XCTAssertEqual(client.requestedURLs, [url])
     }
-    
+
     func test_loadTwice_requestDataFromURLTwice() {
         let url = URL(string: "https://any-url.com/")!
         let (sut, client) = makeSUT(url: url)
@@ -34,17 +34,17 @@ final class RemoteFeedLoaderTest: XCTestCase {
 
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
-    
+
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         client.error = NSError(domain: "Test", code: 0)
-        
-        var capturedError: RemoteFeedLoader.Error?
-        sut.load() { error in capturedError = error }
-        
-        XCTAssertEqual(capturedError, .connectivity)
+
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+
+        XCTAssertEqual(capturedErrors, [.connectivity])
     }
-    
+
     // MARK: - Helpers
 
     private func makeSUT(
@@ -54,11 +54,11 @@ final class RemoteFeedLoaderTest: XCTestCase {
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
     }
-    
+
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
         var error: Error?
-        
+
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             if let error { completion(error) }
             requestedURLs.append(url)
