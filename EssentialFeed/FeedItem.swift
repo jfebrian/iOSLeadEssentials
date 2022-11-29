ceb19f16e5a9fa551d560aaeb1caeb3ea907f97c
Joanda Febrian <mail@joanda.dev>
2022-11-29 19:26:53 +0700

    Create FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Joanda Febrian on 29/11/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
