56aeb8281305355355ba2d8abec8ca5b56eb59e2
Joanda Febrian <mail@joanda.dev>
2022-12-11 14:48:22 +0700

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` overrides items saved by separate instances, proving we correctly manage the data models on disk

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -35,12 +35,37 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         
         let saveExp = expectation(description: "Wait for save completion")
         sutToPerformSave.save(feed) { saveError in
-            XCTAssertNil(saveError)
+            XCTAssertNil(saveError, "Expected to save feed successfully")
             saveExp.fulfill()
         }
         wait(for: [saveExp], timeout: 5)
         
         expect(sutToPerformLoad, toLoad: feed)
+    }
+    
+    func test_save_overridesItemsSavedOnASeparateInstance() {
+        let sutToPerformFirstSave = makeSUT()
+        let sutToPerformLastSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let firstFeed = uniqueImageFeed().models
+        let latestFeed = uniqueImageFeed().models
+        
+        let saveExp1 = expectation(description: "Wait for save completion")
+        sutToPerformFirstSave.save(firstFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp1.fulfill()
+        }
+        wait(for: [saveExp1], timeout: 5)
+        
+        let saveExp2 = expectation(description: "Wait for save completion")
+        sutToPerformLastSave.save(latestFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp2.fulfill()
+        }
+        wait(for: [saveExp2], timeout: 5)
+        
+        expect(sutToPerformLoad, toLoad: latestFeed)
+
     }
     
     // MARK: - Helpers
