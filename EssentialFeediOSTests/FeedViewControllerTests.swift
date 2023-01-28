c87ba1da10b627edeaa4cf14b9e3bc2e9982f58c
Joanda Febrian <mail@joanda.dev>
2023-01-28 16:03:13 +0700

    Feed image view loading indicator is visible while loading image

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -145,9 +145,47 @@ final class FeedViewControllerTests: XCTestCase {
             loader.cancelledImageURLs, [image0.url, image1.url],
             "Expected two cancelled image URL requests once second image is also not visible anymore"
         )
-        
     }
     
+    func test_feedImageViewLoadingIndicator_isVisibleWhileLoadingImage() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [makeImage(), makeImage()])
+
+        let view0 = sut.simulateFeedImageViewVisible(at: 0)
+        let view1 = sut.simulateFeedImageViewVisible(at: 1)
+        XCTAssertEqual(
+            view0?.isShowingImageLoadingIndicator, true,
+            "Expected loading indicator for first view while loading first image"
+        )
+        XCTAssertEqual(
+            view1?.isShowingImageLoadingIndicator, true,
+            "Expected loading indicator for second view while loading second image"
+        )
+
+        loader.completeImageLoading(at: 0)
+        XCTAssertEqual(
+            view0?.isShowingImageLoadingIndicator, false,
+            "Expected no loading indicator for first view once first image loading completes successfully"
+        )
+        XCTAssertEqual(
+            view1?.isShowingImageLoadingIndicator, true,
+            "Expected no loading indicator state change for second view once first image loading completes successfully"
+        )
+
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(
+            view0?.isShowingImageLoadingIndicator, false,
+            "Expected no loading indicator state change for first view once second image loading completes with error"
+        )
+        XCTAssertEqual(
+            view1?.isShowingImageLoadingIndicator,
+            false,
+            "Expected no loading indicator for second view once second image loading completes with error"
+        )
+    }
+
     // MARK: - Helpers
     
     private func makeSUT(
@@ -250,15 +288,31 @@ final class FeedViewControllerTests: XCTestCase {
             func cancel() {
                 cancelCallback()
             }
-            
         }
         
-        private(set) var loadedImageURLs = [URL]()
+        private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+
+        var loadedImageURLs: [URL] {
+            return imageRequests.map { $0.url }
+        }
+
         private(set) var cancelledImageURLs = [URL]()
         
-        func loadImageData(from url: URL) -> FeedImageDataLoaderTask {
-            loadedImageURLs.append(url)
+        func loadImageData(
+            from url: URL,
+            completion: @escaping (FeedImageDataLoader.Result) -> Void
+        ) -> FeedImageDataLoaderTask {
+            imageRequests.append((url, completion))
             return TaskSpy { [weak self] in self?.cancelledImageURLs.append(url) }
+        }
+        
+        func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
+            imageRequests[index].completion(.success(imageData))
+        }
+
+        func completeImageLoadingWithError(at index: Int = 0) {
+            let error = NSError(domain: "an error", code: 0)
+            imageRequests[index].completion(.failure(error))
         }
     }
 }
@@ -309,6 +363,10 @@ private extension FeedImageCell {
     
     var descriptionText: String? {
         descriptionLabel.text
+    }
+    
+    var isShowingImageLoadingIndicator: Bool {
+        feedImageContainer.isShimmering
     }
 }
 
--- EssentialFeediOSTests/Specs.md
+++ EssentialFeediOSTests/Specs.md
@@ -7,6 +7,6 @@
 [ ] Image loading experience
     [V] Load when image view is visible (on screen)
     [V] Cancel when image view is out of screen
-    [ ] Show a loading indicator while loading image (shimmer)
+    [V] Show a loading indicator while loading image (shimmer)
     [ ] Option to retry on image download error
     [ ] Preload when image view is near visible
