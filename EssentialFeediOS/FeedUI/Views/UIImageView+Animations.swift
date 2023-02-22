872454047187f045579287c3ea0b1bb6a2765378
Joanda Febrian <mail@joanda.dev>
2023-02-22 15:15:00 +0700

    Display image with animation

---
+++ EssentialFeediOS/FeedUI/Views/UIImageView+Animations.swift
@@ -0,0 +1,19 @@
+//
+//  UIImageView+Animations.swift
+//  EssentialFeediOS
+//
+//  Created by Joanda Febrian on 22/02/23.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) { self.alpha = 1 }
+    }
+}
