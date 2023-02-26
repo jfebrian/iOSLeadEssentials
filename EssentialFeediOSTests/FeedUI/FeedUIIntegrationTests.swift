950b89424b236d160afae1c291facf408f5bb916
Joanda Febrian <mail@joanda.dev>
2023-02-26 15:46:37 +0700

    Does not render load feed error on load

--- EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
@@ -380,6 +380,14 @@ final class FeedUIIntegrationTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1.0)
     }
+    
+    func test_errorView_doesNotRenderErrorOnLoad() {
+        let (sut, _) = makeSUT()
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(sut.errorMessage, nil)
+    }
 
     // MARK: - Helpers
     
--- EssentialFeediOSTests/FeedUI/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeediOSTests/FeedUI/Helpers/FeedViewController+TestHelpers.swift
@@ -58,4 +58,8 @@ extension FeedViewController {
     }
     
     private var feedImagesSection: Int { 0 }
+    
+    var errorMessage: String? {
+        errorView.message
+    }
 }
