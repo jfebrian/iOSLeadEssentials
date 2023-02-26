148066318afba9e0e96b0a784069505b62f282df
Joanda Febrian <mail@joanda.dev>
2023-02-26 16:09:34 +0700

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeediOS/FeedPresentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Joanda Febrian on 26/02/23.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
