e3f5aa35ead5c95c98a962969521dd814456f3db
Joanda Febrian <jfe@mega.co.nz>
2023-11-25 12:16:57 +0700

    Initial Commit

---
+++ EssentialApp/ViewController.swift
@@ -0,0 +1,14 @@
+// Created by Joanda Febrian. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
