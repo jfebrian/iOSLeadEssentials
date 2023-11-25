3cd170fee45eef6fe85ea6b0d9a3fc8d3cf6120b
Joanda Febrian <mail.joanda.dev>
2023-11-25 15:42:09 +0700

    Displays remote feed on launch when customer has connectivity

--- EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -101,7 +101,11 @@ extension FeedViewController {
         let index = IndexPath(row: row, section: feedImagesSection)
         return ds?.tableView(tableView, cellForRowAt: index)
     }
-    
+
+    func renderedFeedImageData(at index: Int) -> Data? {
+        return simulateFeedImageViewVisible(at: index)?.renderedImage
+    }
+
     private var feedImagesSection: Int { 0 }
     
     var errorMessage: String? {
