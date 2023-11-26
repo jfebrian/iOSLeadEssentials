ed38a923fdff9b3213f69c5e4f21d1cb19f3bf49
Joanda Febrian <mail@joanda.dev>
2023-11-26 13:09:29 +0700

    Displays mapped resource on successful resource loading

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -2,10 +2,17 @@
 
 import Foundation
 
+public protocol ResourceView {
+    func display(_ viewModel: String)
+}
+
 public final class LoadResourcePresenter {
-    private let feedView: FeedView
+    public typealias Mapper = (String) -> String
+
+    private let resourceView: ResourceView
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
+    private let mapper: Mapper
 
     private var feedLoadError: String {
         NSLocalizedString(
@@ -16,10 +23,16 @@ public final class LoadResourcePresenter {
         )
     }
 
-    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
-        self.feedView = feedView
+    public init(
+        resourceView: ResourceView,
+        loadingView: FeedLoadingView,
+        errorView: FeedErrorView,
+        mapper: @escaping Mapper
+    ) {
+        self.resourceView = resourceView
         self.loadingView = loadingView
         self.errorView = errorView
+        self.mapper = mapper
     }
 
     public func didStartLoading() {
@@ -27,8 +40,8 @@ public final class LoadResourcePresenter {
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
 
-    public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(FeedViewModel(feed: feed))
+    public func didFinishLoading(with resource: String) {
+        resourceView.display(mapper(resource))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 
