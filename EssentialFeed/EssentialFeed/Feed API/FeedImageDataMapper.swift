d44a189702589329acd01492a0e298c27cab95d2
Joanda Febrian <mail@joanda.dev>
2023-11-26 12:52:32 +0700

    Test FeedImageDataMapper in isolation

---
+++ EssentialFeed/EssentialFeed/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,17 @@
+// Created by Joanda Febrian. All rights reserved.
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+
+        return data
+    }
+}
