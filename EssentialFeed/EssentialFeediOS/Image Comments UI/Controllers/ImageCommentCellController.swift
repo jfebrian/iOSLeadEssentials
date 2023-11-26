101df1573ced4d38a8313209458554f3790de2b2
Joanda Febrian <mail@joanda.dev>
2023-11-26 14:57:28 +0700

    Add default implementation for optional methods in CellController protocol

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -17,8 +17,4 @@ public class ImageCommentCellController: CellController {
         cell.dateLabel.text = model.date
         return cell
     }
-
-    public func preload() {}
-
-    public func cancelLoad() {}
 }
--- EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -14,6 +14,11 @@ public protocol CellController {
     func cancelLoad()
 }
 
+public extension CellController {
+    func preload() {}
+    func cancelLoad() {}
+}
+
 public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching {
     public var onRefresh: (() -> Void)?
 
