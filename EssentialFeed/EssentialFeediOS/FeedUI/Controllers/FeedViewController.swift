53a405ea2c943f705506f50ae120ca7b89eb749e
Joanda Febrian <mail@joanda.dev>
2023-11-26 14:01:26 +0700

    Add CellController protocol in preparation to support different cell controller types

--- EssentialFeed/EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
@@ -12,6 +12,12 @@ public protocol FeedViewControllerDelegate {
     func didRequestFeedRefresh()
 }
 
+public protocol CellController {
+    func view(in tableView: UITableView) -> UITableViewCell
+    func preload()
+    func cancelLoad()
+}
+
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     public var delegate: FeedViewControllerDelegate?
 
@@ -19,9 +25,9 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
     @IBOutlet private(set) public var errorView: ErrorView?
 
-    private var loadingControllers = [IndexPath: FeedImageCellController]()
+    private var loadingControllers = [IndexPath: CellController]()
 
-    private var tableModel = [FeedImageCellController]() {
+    private var tableModel = [CellController]() {
         didSet { tableView.reloadData() }
     }
     
@@ -50,7 +56,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         delegate?.didRequestFeedRefresh()
     }
 
-    public func display(_ cellControllers: [FeedImageCellController]) {
+    public func display(_ cellControllers: [CellController]) {
         loadingControllers = [:]
         tableModel = cellControllers
     }
@@ -77,7 +83,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         indexPaths.forEach(cancelCellControllerLoad(forRowAt:))
     }
     
-    private func feedImageCellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
+    private func feedImageCellController(forRowAt indexPath: IndexPath) -> CellController {
         let controller = tableModel[indexPath.row]
         loadingControllers[indexPath] = controller
         return controller
