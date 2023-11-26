731dab6ea00a9546060d572ad9ae59e051a63b0e
Joanda Febrian <mail@joanda.dev>
2023-11-26 13:37:58 +0700

    Display error on mapper error

--- EssentialFeed/EssentialFeedTests/Shared Presentation/LoudResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Presentation/LoudResourcePresenterTests.swift
@@ -34,6 +34,19 @@ final class LoadResourcePresenterTests: XCTestCase {
         ])
     }
 
+    func test_didFinishLoadingWithMapperError_displaysLocalizedErrorMessageAndStopsLoading() {
+        let (sut, view) = makeSUT(mapper: { resource in
+            throw anyNSError
+        })
+
+        sut.didFinishLoading(with: "resource")
+
+        XCTAssertEqual(view.messages, [
+            .display(errorMessage: localized("GENERIC_CONNECTION_ERROR")),
+            .display(isLoading: false)
+        ])
+    }
+
     func test_didFinishLoadingWithError_displaysLocalizedErrorMessageAndStopsLoading() {
         let (sut, view) = makeSUT()
 
